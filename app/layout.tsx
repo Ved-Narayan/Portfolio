f346dc494ed2ff657e1f30253c05bb7f00be79ed
Ved Narayan <114128689+Ved-Narayan@users.noreply.github.com>
2025-07-02 12:57:42 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/vn.png" />
+      </head>
       <body>{children}</body>
     </html>
   )
